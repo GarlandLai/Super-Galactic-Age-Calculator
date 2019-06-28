52d2230ad4827c0f71803d225b89cf2ad8a8972d
Garland Lai <garlandlai24@gmail.com>
2019-06-28 11:51:13 -0700

    add function with if statment to find life expectancy

--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -2,6 +2,7 @@
 export class CalculateAge {
   constructor(age) {
     this.age = age;
+    this.questions = questions;
   }
   returnMercury() {
     const mercuryYears = (1/.24);
@@ -22,5 +23,21 @@ export class CalculateAge {
     const jupiterYears = (1/11.86);
     const userAge = new Date(this.age);
     return jupiterYears * userAge
+  }
+  calculateLifeExpectancy() {
+    const normalAge = 80;
+    if (questionChecked = 0) {
+      return 80
+    } else if (questionChecked === 5) {
+      return 80 - 25
+    } else if (questionChecked === 4) {
+      return 80 - 20
+    } else if (questionChecked === 3) {
+      return 80 - 15
+    } else if (questionChecked === 2) {
+      return 80 - 10
+    } else if (questionChecked === 1) {
+      return 80 - 5
+    }
   }
 }
